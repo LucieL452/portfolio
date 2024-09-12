9561b56998e90b295fbdfe51a3a954ed71e29246
LucieL452 <lucie.limoges@gmail.com>
2024-09-12 16:56:26 +0200

    Add files via upload

---
+++ Projet Java Script/scripts/config.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Chocolat", "Rhododendron", "Serviette"]
+const listePhrases = ["Pas de panique a bord !", "Le fun et la vitesse d'abord", "C'est les ratz qui sont la"]
